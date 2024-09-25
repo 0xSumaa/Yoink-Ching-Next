c77aff6534599da40256080280acd17cebc3bb27
0xSumaa <0xSumaa@gmail.com>
2024-09-25 03:50:37 -0700

    display prize pool on site

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { getFrameMetadata } from "frog/next";
 import type { Metadata } from "next";
-
+import { fetchBalance } from "./api/[[...routes]]/utils/on-chain-data";
 import styles from "./page.module.css";
-
+import { formatBalance } from "./api/[[...routes]]/utils/format";
+import { config } from "dotenv";
+config();
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
     `${process.env.VERCEL_URL || "http://localhost:3000"}/api`
@@ -12,10 +14,15 @@ export async function generateMetadata(): Promise<Metadata> {
   };
 }
 
-export default function Home() {
+export default async function Home() {
+  const balance = await fetchBalance(process.env.YOINK_ADDRESS as string);
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Yoink-Ching!</h1>
+      <h2 className={styles.prizePool}>
+        Current Prize Pool: {formatBalance(balance)} MOXIE
+      </h2>
       <div className={styles.linksTitle}>Links</div>
       <div className={styles.links}>
         <a href="https://warpcast.com/sumaa/0x2769eec9">Yoink On Warpcast</a>
